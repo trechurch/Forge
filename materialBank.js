bc31e473aa2200080f359c5cfdd94cfa922572df
trechurch <trechurch@gmail.com>
2025-07-30 09:36:59 -0500

    Create materialBank.js
    part of the launch stack

---
+++ materialBank.js
@@ -0,0 +1,17 @@
+export const materialBank = {
+  earth: null,
+  moon: null,
+  starfield: null,
+
+  assign(id, material) {
+    if (!this.hasOwnProperty(id)) {
+      console.warn(`Unknown material ID: ${id}`);
+      return;
+    }
+    this[id] = material;
+  },
+
+  get(id) {
+    return this[id] || null;
+  }
+};
